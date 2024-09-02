cd16204eff720c005108458a95451f27ea9c28c0
leonardofalango <leo_falango@hotmail.com>
2024-09-02 11:52:37 -0300

    remove file .log to logging in vercel

--- app.py
+++ app.py
@@ -23,8 +23,6 @@ logging.getLogger("app_logger")
 logging.basicConfig(
     datefmt="%Y-%m-%d | %H:%M:%S |",
     level=logging.DEBUG,
-    format="%(asctime)s | %(levelname)s | %(message)s",
-    filename="app.log",
 )
 
 app = FastAPI()
